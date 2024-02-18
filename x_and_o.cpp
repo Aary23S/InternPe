825fba629e50b6212a07e6b6a04ff78040cef545
Aary23S <satardekaraary47@gmail.com>
2024-02-18 20:59:59 +0530

    add new file

--- x_and_o.cpp
+++ x_and_o.cpp
@@ -1,464 +1,137 @@
-//Connect four game
-#include<iostream>
-using namespace std ;
+#include <iostream>
+#include <vector>
 
-//the board
-char arr[6][7]={{'','','','','','',''}, {'','','','','','',''},{'','','','','','',''},{'','','','','','',''},{'','','','','','',''},{'','','','','','',''}};
-	//cheak the places of board
-	int c1=6;
-	int c2=6;
-	int c3=6;
-	int c4=6;
-	int c5=6;
-	int c6=6;
-	int c7=6;
-//function of place of x or o
-int insert(int x)
+using namespace std;
+
+const int ROWS = 6;
+const int COLS = 7;
+
+vector<vector<char>> board(ROWS, vector<char>(COLS, ' '));
+
+void printboard() 
 {
-
-	//cheak values
-	if(x==1){
-		if (c1>0) {
-			c1--;
-			return c1;
-		}
-		else
-		return -1;
-	}
-	else if(x==2){
-		if (c2>0){
-			c2--;
-			return c2;
-		}
-		else
-		return -1;
-	}
-	else if(x==3){
-		if (c3>0){
-			c3--;
-			return c3;
-		}
-		else
-		return -1;
-	} 
-	else if(x==4){
-		if (c4>0)
-		{
-			c4--;
-			return c4;
-		}
-		else
-		return -1;
-	}
-	else if(x==5){
-		if (c5>0){
-			c5--;
-			return c5;
-		}
-		else
-		return -1;	
-	}
-	else if(x==6){	
-		if (c6>0){
-			c6--;
-			return c6;
-		}
-		else
-		return -1;
-	}
-	else if(x==7){
-		if (c7>0){
-			c7--;
-			return c7;
-		}
-		else
-		return -1;	
-	}
-	else 
-	return -1;
-	
+    for (int i = 0; i < ROWS; ++i) 
+    {
+        for (int j = 0; j < COLS; ++j) 
+        {
+            cout << "| " << board[i][j] << " ";
+        }
+        cout << "|" << endl;
+    }
+    cout << "-----------------------------" << endl;
 }
 
-bool win_p1_row (int a)//1st case to win p1
+bool iscolumnfull(int col) 
 {
-	int count=0;
-		for(int j=0; j<7;j++){
-			if (arr[a][j]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-		}
-		return false;
-		
+    return board[0][col] != ' ';
 }
 
-bool win_p1_dai1 (int a , int b)//2nd case to win p1
+bool isvalidmove(int col) 
 {
-	int count=0;
-	b--;
-	if (a>=b)
-	{
-		a-=b;
-		b=0;
-		while(a<6)
-		{
-			if (arr[a][b]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b++;
-		}
-	}
-	else
-	{
-		b-=a;
-		a=0;
-		while(b<7)
-		{
-			if (arr[a][b]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b++;
-		}
-	}
-		return false;
-		
+    return col >= 0 && col < COLS && !iscolumnfull(col);
 }
 
-bool win_p1_dai2 (int a , int b)//3rd case to win p1
+bool checkwin(char player) 
 {
-	int count=0;
-	b--;
-	int c =a+b;
-	if (c<7 && c>2)
-	{
-		b=c;
-		a=0;
-		while(a<6)
-		{
-			if (arr[a][b]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b--;
-		}
-	}
-	else if (c>6 && c<9)
-	{
-		b=6;
-		a=c-6;
-		while(a<6)
-		{
-			if (arr[a][b]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b--;
-		}
-	}
-		return false;
-		
+    // Check horizontal
+    for (int i = 0; i < ROWS; ++i) 
+    {
+        for (int j = 0; j <= COLS - 4; ++j) 
+        {
+            if (board[i][j] == player && board[i][j+1] == player && board[i][j+2] == player && board[i][j+3] == player) 
+            {
+                return true;
+            }
+        }
+    }
+
+    // Check vertical
+    for (int i = 0; i <= ROWS - 4; ++i) 
+    {
+        for (int j = 0; j < COLS; ++j) 
+        {
+            if (board[i][j] == player && board[i+1][j] == player && board[i+2][j] == player && board[i+3][j] == player) 
+            {
+                return true;
+            }
+        }
+    }
+
+    // Check diagonal
+    for (int i = 0; i <= ROWS - 4; ++i) 
+    {
+        for (int j = 0; j <= COLS - 4; ++j) 
+        {
+            if (board[i][j] == player && board[i+1][j+1] == player && board[i+2][j+2] == player && board[i+3][j+3] == player) 
+            {
+                return true;
+            }
+            if (board[i][j+3] == player && board[i+1][j+2] == player && board[i+2][j+1] == player && board[i+3][j] == player) 
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
-bool win_p1_col (int a)//4th case to win p1
-{
-	int count=0;
-	a--;
-		for(int j=0; j<6;j++)
-		{
-			if (arr[j][a]=='X')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-		}
-		return false;
-		
+bool isboardfull() {
+    for (int i = 0; i < ROWS; ++i) 
+    {
+        for (int j = 0; j < COLS; ++j) 
+        {
+            if (board[i][j] == ' ') 
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-bool win_p2_row (int a)//1st case to win p2
+int main() 
 {
-	int count=0;
-	
-		for(int j=0; j<7;j++)
-		{
-			if (arr[a][j]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-		}
-		return false;
-		
-}
+    char currentPlayer = 'X';
+    bool gameover = false;
 
-bool win_p2_col (int a)//2nd case to win p2
-{
-	int count=0;
-	a--;
-		for(int j=0; j<6;j++)
-		{
-			if (arr[j][a]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-		}
-		return false;
-		
-}
+    while (!gameover) 
+    {
+        printboard();
 
-bool win_p2_dai1 (int a , int b)//3rd case to win p2
-{
-	int count=0;
-	b--;
-	if (a>=b)
-	{
-		a-=b;
-		b=0;
-		while(a<6)
-		{
-			if (arr[a][b]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b++;
-		}
-	}
-	else
-	{
-		b-=a;
-		a=0;
-		while(b<7)
-		{
-			if (arr[a][b]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b++;
-		}
-	}
-		return false;
-		
-}
+        int col;
+        cout << "Player " << currentPlayer << ", enter column (0-6) to drop your piece: ";
+        cin >> col;
 
-bool win_p2_dai2 (int a , int b)//4th case to win p2
-{
-	int count=0;
-	b--;
-	int c =a+b;
-	if (c<7 && c>2)
-	{
-		b=c;
-		a=0;
-		while(a<6)
-		{
-			if (arr[a][b]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b--;
-		}
-	}
-	else if (c>6 && c<9)
-	{
-		b=6;
-		a=c-6;
-		while(a<6)
-		{
-			if (arr[a][b]=='O')
-			{
-				count++;
-				if (count==4)
-				return true ;
-			}
-			else
-			count =0;
-			a++;
-			b--;
-		}
-	}
-		return false;
-		
-}
+        if (isvalidmove(col)) 
+        {
+            // Find the lowest empty row in the selected column
+            int row = ROWS - 1;
+            while (row >= 0 && board[row][col] != ' ') 
+            {
+                row--;
+            }
+            board[row][col] = currentPlayer;
 
-void board(int a, int b, char x)//show the board
-{
-	
-	//the move
-	arr[a][b-1]=x;
-	//head of board
-	for(int f=1; f<8;f++)
-	{
-		cout<<f<<'|';
-	}
-	cout<<endl;
-	//the board
-	for(int i=0;i<6;i++)
-	{
-		for(int j =0;j<7;j++)
-		{
-			cout<<arr[i][j]<<'|';
-		}
-		cout<<endl;
-	}
-}
+            if (checkwin(currentPlayer)) 
+            {
+                printboard();
+                cout << "Player " << currentPlayer << " wins!" << endl;
+                gameover = true;
+            } else if (isboardfull()) 
+            {
+                printboard();
+                cout << "It's a tie!" << endl;
+                gameover = true;
+            }
 
-//the main
-int main()
-{
-	cout<<"Welcome To Connect4 !!"<<endl;//welcome message
-	//head of board
-	for(int f=1; f<8;f++)
-	{
-		cout<<f<<'|';
-	}
-	cout<<endl;
-	//the board
-	for(int i=0;i<6;i++)
-	{
-		for(int j =0;j<7;j++)
-		{
-			cout<<arr[i][j]<<'|';
-		}
-		cout<<endl;
-	}
-	int c=0;//counter
-	while(c<21)//loop to take the moves
-	{
-		int p1=0, p2=0;
-		int col=0;
-		int k=0;//row
-		while(p1<1)
-		{
-			cout<<"Player1"<<endl<<"Enter column: ";//take move of p1
-			col=0;
-			cin>>col;
-			 k=insert(col);//calclute the row
-			while (k==-1)//if p1 write invalid place
-			{
-				cout<< "Invalid place, Play Again: ";
-				cin>>col;
-				k=insert(col);
-			}
-			board(k,col,'X');//show the board
-			
-			p1++;
-		}
-		//cheak the p1 is winner or no
-		if (win_p1_row (k)==true)
-			{
-				cout<<"Player 1 is Winner"<<endl<<"Congratulations!!";
-				break;
-			}
-		
-		if (win_p1_col (col)==true)
-			{
-				cout<<"Player 1 is Winner"<<endl<<"Congratulations!!";
-				break;
-			}
-			
-		if (win_p1_dai1 (k,col)==true)
-		{
-			cout<<"Player 1 is Winner"<<endl<<"Congratulations!!";
-			break;
-		}
-		if (win_p1_dai2 (k,col)==true)
-		{
-			cout<<"Player 1 is Winner"<<endl<<"Congratulations!!";
-			break;
-		}
-			
-		while(p2<1)
-		{
-			cout<<"Player2"<<endl<<"Enter column: ";//take move of p2
-			col=0;
-			cin>>col;
-			k=insert(col);//calclute the row
-			while (k==-1)//if p1 write invalid place
-			{
-				cout<< "Invalid place, Play Again: ";
-				cin>>col;
-				k=insert(col);
-			}
-			board(k,col,'O');//show the board
-			p2++;
-		}
-		//cheak the p2 is winner or no
-		if (win_p2_row (k)==true)
-			{
-				cout<<"Player 2 is Winner"<<endl<<"Congratulations!!";
-				break;
-			}
-		if (win_p2_col (col)==true)
-			{
-				cout<<"Player 2 is Winner"<<endl<<"Congratulations!!";
-				break;
-			}
-		if (win_p2_dai1 (k,col)==true)
-		{
-			cout<<"Player 2 is Winner"<<endl<<"Congratulations!!";
-			break;
-		}
-		if (win_p2_dai2 (k,col)==true)
-		{
-			cout<<"Player 2 is Winner"<<endl<<"Congratulations!!";
-			break;
-		}
-		c++;
-		//if No Winner
-		if (c==21)
-		{
-			cout<<"Sorry! No Winner";
-		}
-	}
+            // Switch players
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        } else 
+        {
+            cout << "Invalid move. Please try again." << endl;
+        }
+    }
+
+    return 0;
 }
